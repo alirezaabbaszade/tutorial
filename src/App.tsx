5f550dedc1195b16873a1be48849073af18e5ec3
alireza.ab <alirezaabbaszade05@gmail.com>
2022-07-11 11:21:43 +0430

    first commit:create foolder structure and create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Navbar} from "./components/Navbar";
+import {Container} from "react-bootstrap";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <Navbar/>
+        <Container className="mb-4">
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/store" element={<Store/>}/>
+            </Routes>
+        </Container>
+        </>
+    );
 }
 
 export default App;
